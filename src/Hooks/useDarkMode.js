0dccb6b8eff3a750aa272bf993d1c046589964fe
Wade-Coplen <wadecoplen@gmail.com>
2020-01-23 19:56:58 -0700

    first commit

---
+++ src/Hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+ 
+import useLocalStorage from './useLocalStorage';
+
+function useDarkMode() {
+    useLocalStorage('darkModeOn');
+
+    return thing ? JSON.parse(item) : someValue
+    const setValue = value => {                
+        setStoredValue(value);
+        //updating state to 'value'
+        window.localStorage.setItem(key, 
+        //.setItem() is used to save 'value' to localStorage.
+        //it will add the 'key' to localStorage object or update it if it already exists.
+        JSON.stringify(value));
+        //value is being converted to a JSON string.
+    return [value, setter];
+}
